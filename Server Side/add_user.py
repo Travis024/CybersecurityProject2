0b74c9e5d8910fc8f8fd2b75411b7a6a98aa9430
AliciEdwards <61361680+AliciEdwards@users.noreply.github.com>
2020-02-26 11:59:23 -0700

    Update add_user.py

--- Server Side/add_user.py
+++ Server Side/add_user.py
@@ -10,6 +10,14 @@ import binascii
 
 user = input("Enter a username: ")
 password = input("Enter a password: ")
+level = input("Enter a clearance level (0 for Top Secret, 1 for Secret, 2 for Confidential, 3 for Unclassified:)") #get MAC info
+docclass = []
+cont = 'y'
+while cont == 'y':
+    docclass.append(input("Enter a document class (1, 2, or 3)"))
+    cont = input("Would you like to enter another document class? (y or n)")
+
+
 
 salt = os.urandom(32) #pulls from /dev/urandom, 32 bytes in size
 string_salt = str(binascii.hexlify(salt))[2:-1] #turns the bytes into a string for storing in passfile.txt. DO NOT want to store as bytes
@@ -33,5 +41,8 @@ except FileNotFoundError:
     pass
 
 with open("passfile.txt", 'a+') as writer:
-    writer.write("{0}\t{1}\t{2}\n".format(user, string_salt, string_hashed_password)) #make sure to store the string versions, NOT byte versions
+    writer.write("{0}\t{1}\t{2}\t{3}".format(user, string_salt, string_hashed_password, str(level))) #make sure to store the string versions, NOT byte versions
+    for item in docclass:
+        writer.write("\t{0}".format(str(item))) #adding the document classes our user has access to
+    writer.write("\n")
     print("User successfully added!")
